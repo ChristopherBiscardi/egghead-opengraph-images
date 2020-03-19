af783f47bc08bb7046df48112db4960bef34aa0f
Christopher Biscardi <chris@christopherbiscardi.com>
2020-03-18 21:53:31 -0700

    process-url

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 install:
 	cd functions/gen-opengraph-image && npm i && npm run build
+	cd functions/process-url && npm i
---
+++ functions/process-url/process-url.js
@@ -0,0 +1,38 @@
+const cloudinary = require("cloudinary").v2;
+const qs = require("querystring");
+cloudinary.config({
+  cloud_name: "sector",
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
+
+exports.handler = async function(event, ctx) {
+  const { queryStringParameters } = event;
+
+  try {
+    // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
+    const imageUrl = cloudinary.url(
+      `${process.env.CHRIS_IMAGE_VERSION}/og-images/img-1.png`,
+      {
+        // resouce_type: "raw"
+        sign_url: true,
+        // secure: true,
+        custom_pre_function: {
+          function_type: "remote",
+          source: `https://relaxed-payne-d1bfbe.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+            queryStringParameters
+          )}`
+        }
+      }
+    );
+    return {
+      statusCode: 302,
+      headers: {
+        Location: imageUrl
+      },
+      body: ""
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
