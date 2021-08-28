076dd16c50a116d640dd915aec344ed8d83a564c
Alan W. Smith <alan@alanwsmith.com>
2021-08-28 15:32:46 -0400

    Added Environmental Variables

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const qs = require("querystring");
 cloudinary.config({
-  cloud_name: "sector",
+  cloud_name: process.env.CLOUDINARY_CLOUD,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET
 });
@@ -10,22 +10,21 @@ exports.handler = async function(event, ctx) {
   const { queryStringParameters } = event;
   console.log(queryStringParameters);
   try {
-    // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
     const imageUrl = cloudinary.url(
-      `${process.env.CHRIS_IMAGE_VERSION}/og-images/img-2.png`,
+      `${process.env.CLOUDINARY_BASE_OG_IMAGE}`,
       {
         // resouce_type: "raw"
         sign_url: true,
         // secure: true,
         custom_pre_function: {
           function_type: "remote",
-          source: `https://relaxed-payne-d1bfbe.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+          source: `${process.env.GEN_OPENGRAPH_IMAGE_BASE_URL}?${qs.stringify(
             queryStringParameters
           )}`
         }
       }
     );
-    console.log(`https://relaxed-payne-d1bfbe.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+    console.log(`${process.env.GEN_OPENGRAPH_IMAGE_BASE_URL}?${qs.stringify(
             queryStringParameters
           )}`)
     return {
