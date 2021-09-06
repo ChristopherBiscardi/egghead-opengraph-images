098291ec5168f1e1831323636aed3d5f519a9b69
Alan W. Smith <alan@alanwsmith.com>
2021-09-06 01:16:51 -0400

    Removing signing to try to get cloudinary to work

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -3,35 +3,34 @@ const qs = require("querystring");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD,
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-exports.handler = async function(event, ctx) {
+exports.handler = async function (event, ctx) {
   const { queryStringParameters } = event;
   console.log(queryStringParameters);
   try {
-    const imageUrl = cloudinary.url(
-      `${process.env.CLOUDINARY_BASE_OG_IMAGE}`,
-      {
-        sign_url: true,
-        secure: true,
-        custom_pre_function: {
-          function_type: "remote",
-          source: `${process.env.GEN_OPENGRAPH_IMAGE_BASE_URL}?${qs.stringify(
-            queryStringParameters
-          )}`
-        }
-      }
+    const imageUrl = cloudinary.url(`${process.env.CLOUDINARY_BASE_OG_IMAGE}`, {
+      // sign_url: true,
+      // secure: true,
+      custom_pre_function: {
+        function_type: "remote",
+        source: `${process.env.GEN_OPENGRAPH_IMAGE_BASE_URL}?${qs.stringify(
+          queryStringParameters
+        )}`,
+      },
+    });
+    console.log(
+      `${process.env.GEN_OPENGRAPH_IMAGE_BASE_URL}?${qs.stringify(
+        queryStringParameters
+      )}`
     );
-    console.log(`${process.env.GEN_OPENGRAPH_IMAGE_BASE_URL}?${qs.stringify(
-            queryStringParameters
-          )}`)
     return {
       statusCode: 302,
       headers: {
-        Location: imageUrl
+        Location: imageUrl,
       },
-      body: ""
+      body: "",
     };
   } catch (e) {
     console.log(e);
